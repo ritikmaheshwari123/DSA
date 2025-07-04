eb328620112ccb2fc0542dbf2d87a011b5dbb1ed
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-07-04 14:16:59 +0530

    Sorting 1

---
+++ Practice/src/Sorting/BubbleSortWithRecursion.java
@@ -0,0 +1,42 @@
+package Sorting;
+
+
+public class BubbleSortWithRecursion {
+    static void bubble_sort(int[] arr, int n) {
+        //Base case: range == 1.
+        if (n == 1) return;
+
+        int didSwap = 0;
+        for (int j = 0; j <= n - 2; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                didSwap = 1;
+            }
+        }
+
+        // if no swapping happens.
+        if (didSwap == 0) return;
+
+        //Range reduced after recursion:
+        bubble_sort(arr, n - 1);
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble_sort(arr, n);
+
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
