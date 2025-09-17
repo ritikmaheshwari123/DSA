2e3db2531dbd6a7f3907a05f5e8bfeb49482dd95
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-09-17 08:06:25 +0530

    Easy array problems

---
+++ Practice/src/Array/Easy/MoveZerosToEnd.java
@@ -0,0 +1,29 @@
+package Array.Easy;
+
+public class MoveZerosToEnd {
+    public static void main(String[] args) {
+        int [] nums = {0, 1, 0, 3, 12};
+        int  j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        if(j!=-1){
+            for(int i=j+1;i<nums.length;i++){
+                if(nums[i]!=0){
+                    int temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    j++;
+                }
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
