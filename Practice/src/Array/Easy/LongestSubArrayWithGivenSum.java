654d69f4179ea0d0cf35b31134667209f721c4f3
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-09-28 14:48:42 +0530

    Longest SubArray with given sum

--- Practice/src/Array/Easy/LongestSubArrayWithGivenSum.java
+++ Practice/src/Array/Easy/LongestSubArrayWithGivenSum.java
@@ -1,5 +1,8 @@
 package Array.Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithGivenSum {
     public static void main(String[] args) {
         int [] arr = {10, 5, 2, 7, 1, 9};
@@ -45,6 +48,39 @@ public class LongestSubArrayWithGivenSum {
             // Move forward thw right pointer:
             right++;
             if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static int getLongestSubArrayUsingHashing(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        Map<Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            long rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
         }
 
         return maxLen;
