5da39338ca245b70015b688be216d34864f3dd26
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-10-07 16:31:12 +0530

    Matrix Rotate 90 clockwise

---
+++ Practice/src/Array/Medium/RotateMatrix90Clockwise.java
@@ -0,0 +1,54 @@
+package Array.Medium;
+
+
+public class RotateMatrix90Clockwise {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        matrix = rotateMatOptimal(matrix);
+
+        // Print the rotated matrix
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] rotateMatOptimal(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+        return matrix;
+    }
+
+    private static int[][] rotateMatBrute(int[][] matrix) {
+        //Using extra array
+        int[][] rotated = new int[matrix.length][matrix[0].length];
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+}
