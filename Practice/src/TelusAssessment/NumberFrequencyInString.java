5904573fe83509874c4c03c19138d1965015c8d4
ritikmaheshwari123 <maheshwariritik85@gmail.com>
2025-11-07 22:15:57 +0530

    Telus Assessment

--- Practice/src/TelusAssessment/NumberFrequencyInString.java
+++ Practice/src/TelusAssessment/NumberFrequencyInString.java
@@ -1,16 +1,38 @@
 package TelusAssessment;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class NumberFrequencyInString {
     public static void main(String[] args) {
         String qaz = "1,2,3,4,5,6,6,7,1,5,5,5";
-        Map<String,Long> numberCounts = Arrays.stream(qaz.split(","))
+        withoutStream(qaz);
+
+    }
+
+    public void withStream(String str){
+        Map<String,Long> numberCounts = Arrays.stream(str.split(","))
                 .map(String::trim)
                 .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
 
         numberCounts.forEach((num,count)-> System.out.println(num + ":" + count));
+    }
+
+    public static void withoutStream(String str){
+        String[] numbers = str.split(",");
+
+        // Use a HashMap to store frequency
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        for (String num : numbers) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Print frequencies
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println("Number " + entry.getKey() + " -> " + entry.getValue() + " times");
+        }
     }
 }
